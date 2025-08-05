086ec984c9321aac22c37b1e441e4b741c6a8ed6
KANTNOLI <zahargysev02@gmail.com>
2025-08-05 14:29:27 +0300

    1

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.scss"
+
+function Header() {
+    return (
+        <section className={styles.header}>
+            header
+        </section>
+    );
+}
+
+export default Header;
