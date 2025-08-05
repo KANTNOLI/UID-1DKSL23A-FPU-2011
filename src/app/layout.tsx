4c0bf3045b3f7bacd80b48c17d758c07fa0dae8c
KANTNOLI <zahargysev02@gmail.com>
2025-08-05 14:06:07 +0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Paytone_One, M_PLUS_1, Khula } from "next/font/google";
+import "./globals.css";
+
+const fontPaytone = Paytone_One({
+  variable: "--paytone_One",
+  weight: ["400"],
+  subsets: ["latin"]
+});
+const fontMPLUS1 = M_PLUS_1({
+  variable: "--m_plus_1p",
+  subsets: ["latin"]
+});
+const fontKhula = Khula({
+  variable: "--khula",
+  weight: ["400", "700", "800", "400"],
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  title: "KANTNOLI",
+  description: "KANTNOLI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontPaytone.variable} ${fontMPLUS1.variable} ${fontKhula.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
