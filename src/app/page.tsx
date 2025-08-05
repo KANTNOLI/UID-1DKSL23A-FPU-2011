743522bc81ca170e1eb212224cde14c00ba83556
KANTNOLI <zahargysev02@gmail.com>
2025-08-05 19:25:52 +0300

    icons + footer + pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <section className={styles.main}>    
         <Header />
         <Main />
-        
+        <Footer/>
     </section>
   );
 }
