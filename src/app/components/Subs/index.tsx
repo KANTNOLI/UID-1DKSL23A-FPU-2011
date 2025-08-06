56a2e7b7db26305ca0d6f8072f8d9e2ea4872e34
KANTNOLI <zahargysev02@gmail.com>
2025-08-06 18:00:55 +0300

    back 1

---
+++ src/app/components/Subs/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./Subs.module.scss"
+
+function Subs() {
+    return (
+        <section className={styles.container}>
+            <div className={styles.animBack}>
+                <div className={styles.animBackDiv1}></div>
+                <div className={styles.animBackDiv2}></div>
+                <div className={styles.animBackDiv3}></div>
+
+
+                <div className={styles.animBackDiv4}></div>
+                <div className={styles.animBackDiv5}></div>
+                <div className={styles.animBackDiv6}></div>
+
+                <div className={styles.animBackResult}></div>
+            </div>
+            asd
+        </section>);
+}
+
+export default Subs;
