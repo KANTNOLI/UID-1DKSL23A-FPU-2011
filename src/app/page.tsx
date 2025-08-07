734b76aa5364a5e4630aebdd6b2e6ddca3d3d880
KANTNOLI <zahargysev02@gmail.com>
2025-08-07 10:32:12 +0300

    little work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import styles from "./page.module.scss";
 import Main from "./components/Main";
 import Subs from "./components/Subs";
+import IdeaToWork from "./components/IdeaToWork";
 
 export default function Home() {
   return (
     <section className={styles.main}>
       <Main />
       <Subs />
+      <IdeaToWork />
     </section>
   );
 }
