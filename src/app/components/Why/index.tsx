7a66205f2bfe3bb1ecb5f44cee81dbe0c8cf8670
KANTNOLI <zahargysev02@gmail.com>
2025-08-07 18:01:35 +0300

    ready main why

--- src/app/components/Why/index.tsx
+++ src/app/components/Why/index.tsx
@@ -16,7 +16,39 @@ function Why() {
                 <div className={styles.animBackResult}></div>
             </div>
 
-                <p className={styles.title}>Why Choose Us</p>
+            <div className={styles.cont}>
+                <div className={styles.whyCont}>
+                    <p className={styles.whyContTitle}>Client-first mindset</p>
+
+                    <p className={styles.whyContDesc}>your comfort, not just our profit</p>
+                </div>
+
+                <div className={styles.whyCont}>
+                    <p className={styles.whyContTitle}>Modern design</p>
+
+                    <p className={styles.whyContDesc}>beautiful, user-focused interfaces</p>
+                </div>
+
+                <div className={styles.whyCont}>
+                    <p className={styles.whyContTitle}>Complex? We love it</p>
+
+                    <p className={styles.whyContDesc}>no project is “too hard</p>
+                </div>
+
+                <div className={styles.whyCont}>
+                    <p className={styles.whyContTitle}>Clean code</p>
+
+                    <p className={styles.whyContDesc}>easy to support even without us</p>
+                </div>
+
+                <div className={styles.whyCont}>
+                    <p className={styles.whyContTitle}>Transparency</p>
+
+                    <p className={styles.whyContDesc}>full control stays in your hands</p>
+                </div>
+            </div>
+
+            <p className={styles.title}>Why Choose Us</p>
             <div className={styles.wave}>
             </div>
         </section>
