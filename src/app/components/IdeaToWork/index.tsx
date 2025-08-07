83b3623d690a56ce29cfa158397aaf2e76a6a85d
KANTNOLI <zahargysev02@gmail.com>
2025-08-07 10:32:18 +0300

    add idea page

---
+++ src/app/components/IdeaToWork/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./IdeaToWork.module.scss"
+
+function IdeaToWork() {
+    return (
+        <section className={styles.idea}>
+            asdasdas
+        </section>
+    );
+}
+
+export default IdeaToWork;
