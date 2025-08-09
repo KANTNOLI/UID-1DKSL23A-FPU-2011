736dad7ef7979850a73b93851887b0a43d97cfda
KANTNOLI <zahargysev02@gmail.com>
2025-08-09 22:22:08 +0300

    3

--- src/app/components/Main/Frame3D.ts
+++ src/app/components/Main/Frame3D.ts
@@ -1,4 +1,5 @@
-"use clinet";
+import * as THREE from "three";
+("use clinet");
 
 import { WebGLEngine } from "../../../../Engine/VisualEngineConfigs/WebGLEngine";
 import { DefaultCameraSettings } from "../../../../Engine/Cameras/DefaultCameraSettings";
@@ -6,6 +7,11 @@ import { CreateScene } from "../../../../Engine/OtherScripts/CreateScene";
 import CustomObject from "../../../../Engine/Objects/Snippets/CustomObject";
 import { BoxGeometry } from "../../../../Engine/Objects/Geometry/BoxGeometry";
 import { BasicMaterial } from "../../../../Engine/Objects/Materials/BasicMaterial";
+import { CreateModel } from "../../../../Engine/OtherScripts/CreateModel";
+import { DirectionalLightCfg } from "../../../../Engine/Lighting/DirectionalLightCfg";
+import { OrbitControl } from "../../../../Engine/PlayerActions/OrbitControl";
+import { DEGREE } from "../../../../Engine/Constants.interface";
+import { GLTF, GLTFLoader } from "three/examples/jsm/Addons.js";
 
 interface props {
   canvas: HTMLDivElement;
@@ -17,26 +23,41 @@ function Frame3D({ canvas, width, height }: props) {
   const scene = new CreateScene();
   scene.scene.background = null;
 
+  const l = DirectionalLightCfg(scene.scene, { x: 10, y: 10, z: 10 });
+  scene.addScene(l);
+
   const renderer = WebGLEngine({}, { height, width });
   renderer.setClearColor(0x000000, 0);
 
-  const cube = CustomObject(BoxGeometry(), BasicMaterial({ color: 0xff0000 }));
-  scene.addScene(cube);
-
   const camera = DefaultCameraSettings(
-    { x: 1, y: 1, z: 2 },
+    { x: 0, y: 1, z: 3.8 },
     { aspect: width / height }
   );
-  camera.lookAt(cube.position);
+
+  let loadedModel: THREE.Group;
 
   canvas.appendChild(renderer.domElement);
 
+  const modelsLoader = new GLTFLoader();
+  modelsLoader.load("http://172.20.10.3:3000/main2.glb", (model: GLTF) => {
+    if (!model) {
+      throw new Error("Failed to load model");
+    }
+    loadedModel = model.scene;
+
+    scene.addScene(loadedModel);
+    scene.scene.lookAt(0, 0, 0);
+
+    loadedModel.rotation.set(0 * DEGREE, 100 * DEGREE, 0 * DEGREE);
+  });
   const animate = () => {
     console.log(123);
 
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
-    cube.rotation.x += 0.01;
+    if (loadedModel) {
+      loadedModel.rotation.y += 0.01;
+      //   loadedModel.rotation.z += 0.01;
+      //   loadedModel.rotation.x += 0.01;
+    }
 
     renderer.render(scene.scene, camera);
     requestAnimationFrame(animate);
