3ff4c05a53ab6a01c487201fee9ebc860fb66083
KANTNOLI <zahargysev02@gmail.com>
2025-09-17 03:41:51 +0300

    1

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -5,19 +5,19 @@ function Footer() {
         <footer className={styles.footer}>
             <p className={styles.footerTitle}>Kontaktieren Sie uns</p>
             <div className={styles.footerContacts}>
-                <a className={styles.footerContactsImg} href="https://fonts.google.com/specimen/Paytone+One" target="_blank" rel="noopener noreferrer">
+                <a className={styles.footerContactsImg} href="https://www.facebook.com/share/g/1C5VcFpvnv/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer">
                     <img src="./contacts/Facebook.png" alt="Facebook" />
                 </a>
- 
-                <a className={styles.footerContactsImg} href="http://#" target="_blank" rel="noopener noreferrer">
+
+                <a className={styles.footerContactsImg} href="https://t.me/KANTNOLI" target="_blank" rel="noopener noreferrer">
                     <img src="./contacts/Telegram.png" alt="Telegram" />
                 </a>
 
-                <a className={styles.footerContactsImg} href="http://#" target="_blank" rel="noopener noreferrer">
+                <a className={styles.footerContactsImg} href="https://www.instagram.com/chazen.co?igsh=bDd5Y3g4aTBvOG44&utm_source=qr" target="_blank" rel="noopener noreferrer">
                     <img src="./contacts/Instagram.png" alt="Instagram" />
                 </a>
 
-                <a className={styles.footerContactsImg} href="http://#" target="_blank" rel="noopener noreferrer">
+                <a className={styles.footerContactsImg} href="https://www.linkedin.com/company/chazen-official" target="_blank" rel="noopener noreferrer">
                     <img src="./contacts/LinkedIn.png" alt="LinkedIn" />
                 </a>
             </div>
