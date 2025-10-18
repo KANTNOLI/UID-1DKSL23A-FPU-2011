c47a61559107c16505f97fab404d5f7f0e148818
KANTNOLI <zahargysev02@gmail.com>
2025-10-18 13:55:53 +0300

    1

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -6,6 +6,8 @@ import Navigate from "./navigate";
 
 import styles from "./Header.module.scss"
 
+import { motion } from "framer-motion"
+
 function Header() {
     const [Mobile, setMobile] = useState<boolean>(false)
     const [MenuActive, setMenuActive] = useState<boolean>(false)
@@ -25,9 +27,15 @@ function Header() {
                     <p className={MenuActive ? styles.menuP3Active : ""}></p>
                 </div>}
 
-            <Navigate Mobile={Mobile} Active={MenuActive} setActive={() => {setMenuActive(false)}}/>
+            <Navigate Mobile={Mobile} Active={MenuActive} setActive={() => { setMenuActive(false) }} />
 
-            <img className={styles.headerLogo} src="./logo.jpg" alt="logo.jpg" />
+            <motion.img
+                initial={{ opacity: 0, x: 25 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+
+                className={styles.headerLogo} src="./logo.jpg" alt="logo.jpg" />
         </section>
     );
 }
