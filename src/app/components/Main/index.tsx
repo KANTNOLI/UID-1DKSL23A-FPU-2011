1d69ae0e79504a72ee810f814b7c55b201452534
KANTNOLI <zahargysev02@gmail.com>
2025-10-18 13:55:57 +0300

    1

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -6,6 +6,9 @@ import Frame3D from "../Frame3D";
 
 import styles from "./Main.module.scss"
 
+import { motion } from "framer-motion"
+import Typed from 'typed.js';
+
 function Main() {
     const [Sizes, setSizes] = useState<DOMRect>()
 
@@ -15,16 +18,53 @@ function Main() {
         setSizes(canvas.current?.getBoundingClientRect())
     }, [canvas])
 
+    const title = useRef(null);
+    useEffect(() => {
+        const typed = new Typed(title.current, {
+            strings: ['Chazen'],
+            typeSpeed: 70,
+            startDelay: 500,
+        });
+
+        return () => typed.destroy();
+    }, []);
+
+
     return (<section className={styles.main}>
-        <p className={styles.mainTitle}>Chazen</p>
-        <div ref={canvas} className={styles.mainCanvas}>
-            <Frame3D position={{x: 0, y: 47, z: 80}} height={Sizes?.height} width={Sizes?.width} modelPath="./women.glb"></Frame3D>
-        </div>
+        <motion.p
+            initial={{ opacity: 0, y: -25 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
 
-        <p className={styles.mainDescL}>Ihr Ziel ist unsere Mission. Wir kümmern uns um jedes Detail, damit Sie sich auf das Wesentliche konzentrieren können</p>
+            ref={title}
+            className={styles.mainTitle}></motion.p>
+        <motion.div
+            initial={{ opacity: 0, y: 25 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+
+            ref={canvas} className={styles.mainCanvas}>
+            <Frame3D position={{ x: 0, y: 47, z: 80 }} height={Sizes?.height} width={Sizes?.width} modelPath="./women.glb"></Frame3D>
+        </motion.div>
+
+        <motion.p
+            initial={{ opacity: 0, x: -25 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+
+            className={styles.mainDescL}>Ihr Ziel ist unsere Mission. Wir kümmern uns um jedes Detail, damit Sie sich auf das Wesentliche konzentrieren können</motion.p>
 
 
-        <p className={styles.mainDescR}>Von der ersten Idee bis zur perfekten Lösung: Ihr Vision führt uns zu außergewöhnlichen Ergebnissen</p>
+        <motion.p
+            initial={{ opacity: 0, x: 25 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+
+            className={styles.mainDescR}>Von der ersten Idee bis zur perfekten Lösung: Ihr Vision führt uns zu außergewöhnlichen Ergebnissen</motion.p>
     </section>);
 }
 
