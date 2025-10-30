aa28247cc565c42d3e7051cff0ed13de5dea0963
KANTNOLI <zahargysev02@gmail.com>
2025-10-30 19:08:34 +0300

    1

--- src/app/components/Subs/SubCarts.tsx
+++ src/app/components/Subs/SubCarts.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
