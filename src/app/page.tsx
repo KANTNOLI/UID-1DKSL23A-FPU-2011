fa053176e655fdcd28061324d1e1d851c287bd1b
KANTNOLI <zahargysev02@gmail.com>
2025-11-15 17:58:09 +0300

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
 'use client'
 
-import Subs from "./components/Subs";
-import IdeaToWork from "./components/IdeaToWork";
-import Why from "./components/Why";
-
-import styles from "./page.module.scss";
-import { useEffect } from "react";
-import { BreadcrumbData } from "./breadcrumb-data";
+import { lazy, Suspense } from "react"
+import Subs from "./components/Subs"
+import IdeaToWork from "./components/IdeaToWork"
+import Why from "./components/Why"
+import styles from "./page.module.scss"
+import { BreadcrumbData } from "./breadcrumb-data"
 
 export default function Home() {
-  useEffect(() => {
-    document.title = "Chazen | Web Design & Development Agency"
-  }, [])
-
   return (
     <section className={styles.main}>
       <BreadcrumbData pathname="/" />
-
+      
       <Subs />
       <IdeaToWork />
+      
+      {/* Оберни Why в Suspense если он тоже тяжелый */}
       <Why />
     </section>
-  );
+  )
 }
-
